ece406b2e3ad8a529e595ba1808f5b1d4bddaf43
DThielke <dave.thielke@gmail.com>
2011-03-20 21:10:26 -0400

    Updated to 4.7.5. Fixed command specific help. Added global toggle to /ch toggle.

--- src/com/herocraftonline/dthielke/herochat/command/commands/ToggleCommand.java
+++ src/com/herocraftonline/dthielke/herochat/command/commands/ToggleCommand.java
@@ -10,30 +10,38 @@ import com.herocraftonline.dthielke.herochat.command.BaseCommand;
 
 public class ToggleCommand extends BaseCommand {
 
+    private boolean allEnabled = true;
+
     public ToggleCommand(HeroChat plugin) {
         super(plugin);
         name = "Toggle";
         description = "Temporarily enables or disables a channel";
-        usage = "§e/ch toggle §9<channel>";
-        minArgs = 1;
+        usage = "§e/ch toggle §8[channel]";
+        minArgs = 0;
         maxArgs = 1;
         identifiers.add("ch toggle");
+        notes.add("§cNote: §eIf no channel is provided, all channels are toggled");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
         ChannelManager cm = plugin.getChannelManager();
-        Channel channel = cm.getChannel(args[0]);
-        if (channel != null) {
+        if (args.length == 0) {
             if (sender instanceof Player) {
                 Player muter = (Player) sender;
-                if (muter == null || plugin.getPermissions().isAdmin(muter) || channel.getModerators().contains(muter.getName())) {
-                    if (channel.isEnabled()) {
-                        channel.setEnabled(false);
-                        sender.sendMessage(plugin.getTag() + "§cDisabled " + channel.getCName());
+                if (muter == null || plugin.getPermissions().isAdmin(muter)) {
+                    if (allEnabled) {
+                        for (Channel c : cm.getChannels()) {
+                            c.setEnabled(false);
+                        }
+                        allEnabled = false;
+                        sender.sendMessage(plugin.getTag() + "§cDisabled all channels");
                     } else {
-                        channel.setEnabled(true);
-                        sender.sendMessage(plugin.getTag() + "§cEnabled " + channel.getCName());
+                        for (Channel c : cm.getChannels()) {
+                            c.setEnabled(true);
+                        }
+                        allEnabled = true;
+                        sender.sendMessage(plugin.getTag() + "§cEnabled all channels");
                     }
                 } else {
                     muter.sendMessage(plugin.getTag() + "§cYou do not have sufficient permission");
@@ -42,7 +50,27 @@ public class ToggleCommand extends BaseCommand {
                 sender.sendMessage(plugin.getTag() + "§cYou must be a player to use this command");
             }
         } else {
-            sender.sendMessage(plugin.getTag() + "§cChannel not found");
+            Channel channel = cm.getChannel(args[0]);
+            if (channel != null) {
+                if (sender instanceof Player) {
+                    Player muter = (Player) sender;
+                    if (muter == null || plugin.getPermissions().isAdmin(muter) || channel.getModerators().contains(muter.getName())) {
+                        if (channel.isEnabled()) {
+                            channel.setEnabled(false);
+                            sender.sendMessage(plugin.getTag() + "§cDisabled " + channel.getCName());
+                        } else {
+                            channel.setEnabled(true);
+                            sender.sendMessage(plugin.getTag() + "§cEnabled " + channel.getCName());
+                        }
+                    } else {
+                        muter.sendMessage(plugin.getTag() + "§cYou do not have sufficient permission");
+                    }
+                } else {
+                    sender.sendMessage(plugin.getTag() + "§cYou must be a player to use this command");
+                }
+            } else {
+                sender.sendMessage(plugin.getTag() + "§cChannel not found");
+            }
         }
     }
 
